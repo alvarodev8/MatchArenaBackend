02a66876876770ddba459effcbae4e5a915acd75
alvarodev8 <alvarodev8@gmail.com>
2025-05-15 01:13:17 +0200

    Remove automatic migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,5 @@ chown -R www-data:www-data /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage
 chmod -R 775 /var/www/bootstrap/cache
 
-# Ejecutar migraciones de Laravel
-php /var/www/artisan migrate --force
-
 # Iniciar PHP-FPM
 exec php-fpm
