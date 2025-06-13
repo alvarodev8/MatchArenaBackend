b5bd135cadf9b1472b082ccb4606d0163595c85d
alvarodev8 <alvarodev8@gmail.com>
2025-06-13 16:09:43 +0200

    Add Pitch model with soft delete and relationship

--- app/Models/Pitch.php
+++ app/Models/Pitch.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pitch extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $fillable = ['name', 'location', 'establishment_id'];
+    protected $fillable = [
+        'establishment_id',
+        'name',
+        'location',
+        'price',
+        'description',
+    ];
+
+    protected $dates = ['deleted_at'];
 
     public function establishment()
     {
